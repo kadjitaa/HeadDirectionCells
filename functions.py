42fa40d7e7e7f56c82a15717fa4df4111cb833af
kadjita <k.asumbisa@gmail.com>
2020-08-27 10:54:51 -0400

    sleepfxn added

--- functions.py
+++ functions.py
@@ -23,6 +23,26 @@ Utilities functions
 Feel free to add your own
 '''
 
+
+def refineSleepFromAccel(acceleration, sleep_ep):
+	vl = acceleration[0].restrict(sleep_ep)
+	vl = vl.as_series().diff().abs().dropna()	
+	a, _ = scipy.signal.find_peaks(vl, 0.025)
+	peaks = nts.Tsd(vl.iloc[a])
+	duration = np.diff(peaks.as_units('s').index.values)
+	interval = nts.IntervalSet(start = peaks.index.values[0:-1], end = peaks.index.values[1:])
+
+	newsleep_ep = interval.iloc[duration>15.0]
+	newsleep_ep = newsleep_ep.reset_index(drop=True)
+	newsleep_ep = newsleep_ep.merge_close_intervals(100000, time_units ='us')
+
+	#newsleep_ep	= sleep_ep.intersect(newsleep_ep)
+
+	return newsleep_ep
+
+
+
+
 #########################################################
 # CORRELATION
 #########################################################
